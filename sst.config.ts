01f01dd0ea200dbfa2dc0982952e027c0b29cdc0
lloyd <lloyd@bymiles.co.uk>
2024-05-04 11:46:07 +0100

    Import /infra to define a single Lambda that returns 200

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,48 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+/**
+ * ## Big Transit monorepo
+ *
+ * A TypeScript monorepo that deploys a database and an API to AWS.
+ *
+ * #### Project structure
+ *
+ * The app is split into separate `packages/` and an `infra/` directory.
+ * 
+ * ```txt {2}
+ * bigtransit
+ * ├─ sst.config.ts
+ * ├─ package.json
+ * ├─ packages
+ * │  └─ functions
+ * └─ infra
+ * ```
+ *
+ * The `packages/` directory includes:
+ *
+ * - `functions/`
+ *
+ *   This directory includes our API Lambda functions. 
+ *
+ *  #### Infrastructure
+ *
+ *  The `infra/` directory logically splits the infrastructure for Big Transit into
+ *  separate files.
+ *
+ *  We have an `api.ts` that exports resources that can be used in any other future
+ *  infrastructure files.
+ *
+ *  They are also re-exported in the `infra/index.ts` file.
+ *
+ *  ```ts title="infra/index.ts"
+ *  export * from "./api";
+ *  ```
+ *
+ *  To use them in the `sst.config.ts` we do a dynamic import. This ensures that the
+ *  infrastructure is only created in the `run` function.
+ *
+ */
+
 export default $config({
   app(input) {
     return {
@@ -8,5 +51,11 @@ export default $config({
       home: "aws",
     };
   },
-  async run() {},
+  async run() {
+    const infra = await import("./infra");
+
+    return {
+      api: infra.api.url,
+    };
+  },
 });

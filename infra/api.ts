6d58474de2be4482d18eeeac138501610f3cac6b
Lloyd Holman <lloydholman@Lloyds-MacBook-Pro.local>
2024-05-25 17:02:15 +0100

    Hook up API to fromID function

--- infra/api.ts
+++ infra/api.ts
@@ -3,7 +3,7 @@ import { database } from "./database";
 export const api = new sst.aws.ApiGatewayV2("BigTransitApi")
   api.route("GET /teams/{teamId}", {  
     link: [database],
-    handler: "./packages/functions/src/team.getTeam",
+    handler: "./packages/functions/src/team.fromID",
   });
   api.route("POST /teams", {
     link: [database],

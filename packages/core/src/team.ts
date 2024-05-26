95fa7b6c8413e572962d031934e077b8bdfab669
Lloyd Holman <lloydholman@Lloyds-MacBook-Pro.local>
2024-05-26 09:04:19 +0100

    Define interface for Team DynamoDB data structure

--- packages/core/src/team.ts
+++ packages/core/src/team.ts
@@ -1,8 +1,16 @@
 export * as Team from "./team"
 
-export interface Info {
+interface Team {
     teamID: string;
     name: string;
+}
+
+interface Item {
+    PK: string;
+    SK: string;
+    Name: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 export function newTeamWithName(_name: string) {
@@ -10,14 +18,14 @@ export function newTeamWithName(_name: string) {
     return {
         teamID: "1",
         name: _name 
-    } as Info
+    } as Team
 }
 
 export function fromID(_teamID: number) {
     return {
         teamID: "1",
         name: "Retention Team" 
-    } as Info
+    } as Team
 }
 
 export function list() {

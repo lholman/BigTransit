9569f3fb4cb414528aaa6fd2548a782b03ba3e21
Lloyd Holman <lloydholman@Lloyds-MacBook-Pro.local>
2024-05-28 17:27:15 +0100

    Unit tests now mock DynamoDB and SST and are FAST and decoupled

--- packages/core/test/team.test.ts
+++ packages/core/test/team.test.ts
@@ -1,19 +1,31 @@
-import { expect, it, describe } from "vitest";
+import { expect, it, describe, beforeEach, vi } from "vitest";
 import { Team } from "@bigtransit/core/team";
-import { validate as uuidValidate } from 'uuid';
+import { mockClient } from "aws-sdk-client-mock";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import {  } from "@aws-sdk/lib-dynamodb";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
+
+vi.mock("sst", () => ({
+  Resource: {
+    BigTransit: {
+      name: "mocked-table-name"
+    }
+  }
+}));
+
+beforeEach(() => {
+    ddbMock.reset();
+  });
 
 describe('Team domain', function() {
-    it("Creates a team with UUID given a team name", async () => {
-        const teamName = "Stream aligned team";
-        const team = await Team.newTeamWithName(teamName);
+    it("should get team by ID from DynamoDB", async () => {
 
-        expect(uuidValidate(team.PK.split('#')[1])).toBe(true);
-        expect(team.name).toBe(teamName);
-    })
+        ddbMock.on(GetCommand).resolves({
+          Item: { teamId: "f25e22a1-1924-4c87-bb7f-46b6d087d80f", name: "Stream aligned team" },
+        });
 
-    it("gets a team by ID", async () => {
         const team = await Team.fromID("f25e22a1-1924-4c87-bb7f-46b6d087d80f");
-
-        expect(team.name).toBe("Stream aligned team");   
-    })
+        expect(team.name).toBe("Stream aligned team");
+    });
 });
